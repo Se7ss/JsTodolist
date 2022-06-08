390b1bf2d77ca024873fbb50b84cab98055ca192
Se7ss <se7ss@siffin.co.nz>
2022-06-08 22:51:17 +0300

    Add delete todo functionality

--- app.js
+++ app.js
@@ -1,12 +1,68 @@
-let btn = document.querySelector('.todo-button');
-btn.addEventListener('click',clickhandler);
+//Selectors
+
+const todoInput = document.querySelector(".todo-input");
+const todoBtn = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 
-function clickhandler(event){
+//Event listeners 
+todoBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', checkDelete);
+//Functions
 
-    input = document.querySelector('.todo-input').value;
-    console.log(input)
+function addTodo(event) {
+    event.preventDefault();
+    let input = todoInput.value;
+    // console.log(input);
+
+    //create todoDiv
+    let todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    let listItem = document.createElement('li');
+    listItem.classList.add('todo-item');
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    let checkedButton = document.createElement('button');
+    checkedButton.classList.add('checked-button');
+    checkedButton.innerHTML = '<i class="fa-solid fa-check"></i></i>';
+
+    listItem.innerHTML = input;
+
+    todoDiv.append(listItem, checkedButton, deleteButton);
+    todoList.append(todoDiv);
+
+    todoInput.value = ""
 
 }
+
+
+function checkDelete(e) {
+
+    console.log(e.target)
+    console.log(e.target.classList.value)
+
+    if (e.target.classList[0] === 'delete-button') {
+
+        e.target.parentElement.remove();
+    }
+    if (e.target.classList[0] === 'checked-button') {
+
+        e.target.parentElement.classList.toggle('todo-completed');
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
